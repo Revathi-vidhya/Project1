e7e923d699136b68e63dfb8dd6eeebd9b5cdda85
Revathi-vidhya <revathividhya54@gmail.com>
2022-07-10 15:38:40 +0530

    Revathi code done

---
+++ src/org/test/GitTest1.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class GitTest1 {
+	private void test1() {
+		// TODO Auto-generated method stub
+System.out.println("test1");
+	}
+public static void main(String[] args) {
+	GitTest1 g = new GitTest1();
+	g.test1();
+}
+}
